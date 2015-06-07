f00f8587378b643a5613f8892ef82d4cdd68eb0a
Your Name <you@example.com>
2015-06-07 15:26:37 -0700

    TriangleCounting for 30 different states

--- triangle_counting.scala
+++ triangle_counting.scala
@@ -11,10 +11,36 @@ import math.abs
 14988215        99483139
 40693681        29253944
 */
+class DSV (var line:String="", var delimiter:String=",",var parts:Array[String]=Array("")) extends Serializable {
+        parts=line.split(delimiter,-1)
+        def hasValidVal(index: Int):Boolean={
+                return (parts(index)!=null)&&(parts.length>index)
+        }
+        def contains(text:String):Boolean={
+                for(i <- 1 to (parts.length-1))
+                        if(parts(i).contains(text))
+                                return false
+                true
+        }
+        override def toString():String={
+                var rep:String=""
+                for(i <- 0 to (parts.length-1)){
+                        rep=rep+parts(i)
+                        if (i!=(parts.length -1))
+                                rep=rep+","
+                }
+                rep=rep+"\n"
+                rep
+        }
+}
 
+var month="0603"//get it as command line arg
 
+var districts=sc.textFile("Rwanda_In/Districts.csv").filter(line=>(line.contains("ID")==false)).map(line=>(new DSV(line,","))).map(d=>(d.parts(1))).distinct.collect()
 
-val graph = GraphLoader.edgeListFile(sc, "Rwanda_Out/EdgeList_hd-0604.csv", true).partitionBy(PartitionStrategy.RandomVertexCut)
+districts.foreach(d=>({
+
+val graph = GraphLoader.edgeListFile(sc, "Rwanda_In/NetworkGraph/EdgeList"+month+"-"+d+".csv", true).partitionBy(PartitionStrategy.RandomVertexCut)
 // Find the triangle count for each vertex
 //The documentation says that the canonical orientation must be true for connected components algorithm but this thread says that it is an overstatement
 //http://mail-archives.apache.org/mod_mbox/spark-dev/201501.mbox/%3CCAPh_B=aiXpoeRAnHs1K6vZRiEqg_fmSMhr5rudqvvF6hzVRraw@mail.gmail.com%3E
@@ -23,26 +49,21 @@ val graph = GraphLoader.edgeListFile(sc, "Rwanda_Out/EdgeList_hd-0604.csv", true
 
 val triCounts = graph.triangleCount().vertices
 
-triCounts.saveAsTextFile("Rwanda_Out/Triangle_Counting-0604-hd.csv")
+triCounts.saveAsTextFile("Rwanda_Out/TriangleCounts/Triangle_Counting-"+month+"-"+d+".csv")
 
+}))
 
-val graph = GraphLoader.edgeListFile(sc, "Rwanda_Out/EdgeList_hp-0604.csv", true).partitionBy(PartitionStrategy.RandomVertexCut)
+//districts.foreach(d=>({
+
+val graph = GraphLoader.edgeListFile(sc, "Rwanda_In/NetworkGraph/EdgeList"+month+"-KigaliNetwork.csv", true).partitionBy(PartitionStrategy.RandomVertexCut)
 // Find the triangle count for each vertex
+//The documentation says that the canonical orientation must be true for connected components algorithm but this thread says that it is an overstatement
+//http://mail-archives.apache.org/mod_mbox/spark-dev/201501.mbox/%3CCAPh_B=aiXpoeRAnHs1K6vZRiEqg_fmSMhr5rudqvvF6hzVRraw@mail.gmail.com%3E
+//Basically this thread says that if there are more than one edges between the two vertices then the srcId<destId
+//To ensure this thing, the python script undirectionality_enforcer should be used
+
 val triCounts = graph.triangleCount().vertices
 
-triCounts.saveAsTextFile("Rwanda_Out/Triangle_Counting-0604-hp.csv")
+triCounts.saveAsTextFile("Rwanda_Out/TriangleCounts/Triangle_Counting-"+month+"-network-kigali.csv")
 
-
-
-val graph = GraphLoader.edgeListFile(sc, "Rwanda_Out/EdgeList_kg-0604.csv", true).partitionBy(PartitionStrategy.RandomVertexCut)
-// Find the triangle count for each vertex
-val triCounts = graph.triangleCount().vertices
-
-triCounts.saveAsTextFile("Rwanda_Out/Triangle_Counting-0604-kg.csv")
-
-
-val graph = GraphLoader.edgeListFile(sc, "Rwanda_Out/EdgeList_nkg-0604.csv", true).partitionBy(PartitionStrategy.RandomVertexCut)
-// Find the triangle count for each vertex
-val triCounts = graph.triangleCount().vertices
-
-triCounts.saveAsTextFile("Rwanda_Out/Triangle_Counting-0604-nkg.csv")
+//}))
